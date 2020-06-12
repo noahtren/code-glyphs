9013dd854fc8736c19564239be698873d9c32ba4
Noah Trenaman <noahtren@gmail.com>
2020-06-12 14:53:49 -0400

    testing codeBERTa

--- debug_language.py
+++ debug_language.py
@@ -11,3 +11,13 @@ if __name__ == "__main__":
   )
   example = "for i in <mask>(10): print(i)"
   print(fill_mask(example))
+
+  prompt = "def sum(a, "
+  prompt_ids = tokenizer.encode(prompt, return_tensors='tf')
+  result = model.generate(
+    prompt_ids,
+    do_sample=True,
+    max_length=50,
+    top_k=5
+  )
+  print(tokenizer.decode(result[0]))
